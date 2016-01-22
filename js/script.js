d9209c91332351fba7c70e9315dbce48dc6ab7ee
Waka867 <ledwing.hernandez@gmail.com>
2016-01-22 17:34:27 -0500

    Feat: NY Times API lookup/AJAX request mostly works
    - Got AJAX request to return the correct object
    - Figured out how to send a query filtered by the state and city that is
    entered by the user
    - Added file for API key
    - Added new entry dropdown for users to choose a state along with
    corresponding value in html
    - Used .each jquery object to iterate through and add things as list
    items, although that functionality is currently limited to the values
    entered (snippet)
    - Changed CSS so that nytimes-articles section has white bkg and only
    takes up 75% of the page max (space wor wiki links)

--- js/script.js
+++ js/script.js
@@ -13,16 +13,39 @@ function loadData() {
 
     // load streetview
 
-
-    // YOUR CODE GOES HERE!
     var street = $("#street").val();
     var city = $("#city").val();
+    var state = $("#state").val();
+
+    // console.log(state); /* Test function */
 
     var addressString = street + city;
 
     $body.append("<img class='bgimg' src='https://maps.googleapis.com/maps/api/streetview?size=1920x1080&pitch=-10&fov=110&location=" + addressString + "'>");
 
     $greeting.text("You want to live at " + street + " " + city + "?");
+
+    // NYT request
+
+    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?[q=" + city + " " + state + "&fq=headline: (" + city + ")&api-key=a767593b8d29d68d271284c53c21504a:4:74049866", function(data) {
+
+        var snippet = data.response.docs[0].snippet;
+
+        console.log(snippet);
+
+        var items = [];
+
+        $.each( data, function( key, val ) {
+            items.push( "<li id='" + key + "'>" + val + snippet + "</li>" );
+        });
+
+        $( "<ul/>", {
+        "class": "nytimes-articles",
+        html: items.join( "" )
+        }).appendTo( "body" );
+
+
+    });
 
     return false;
 };
