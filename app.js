35510036b429c92ab1d12ef48fbccc3acc2242b8
Waka867 <ledwing.hernandez@gmail.com>
2016-03-10 17:47:23 -0500

    functionality additions
    - added click knockout function
    - declared variables for DOM elements
    - organized M V VM structure
    - added a few test functions to execute on submit-btn click
    - added state dropdown menu
    - added footer info
    - moved scripts to bottom of the page
    - added css styling for footer, location list and page title

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 $(document).ready(function(){
 
-	var dude = {
-		age: ko.observable(3)
+	var body = $("body");
+	var address = $("#address").val();
+	var zip = $("#zip").val();
+	var state = $("#state").val();
+	var map = $("#map");
+	var locationList = $("#location-list");
+	var formContainer = $("#form-container");
+
+	/* -------------------------- App data model below ------------------------------ */
+	var model = function() {
+		// address: ko.observable(5),
+		this.address = address;
 	};
 
-	ko.applyBindings(dude);
+	/* -------------------------- App View below ------------------------------ */
+	var view = {
+
+	};
+
+	/* -------------------------- App Controller below ------------------------------ */
+	var viewmodel = {
+
+		testFunction: function() {
+			console.log("chirp testFunction");
+			// console.log(model.address);
+		}
+
+        // clear out old data before new request
+
+        // $wikiElem.text("");
+        // $nytElem.text("");
+
+        // load map?
+	};
+
+	$("#submit-btn").click(function() {
+		console.log("button click line 42");
+	});
+
+	ko.applyBindings(viewmodel);
+
+	// formContainer.submit(viewmodel);
+
+	// $("#submit-btn").submit(viewmodel);
 
 });
