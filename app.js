00c5522e539afc9c8f105322e61f6b9be317557f
Waka867 <ledwing.hernandez@gmail.com>
2016-03-11 14:52:08 -0500

    Redo structure, more testing
    - removed AFRlogo test img
    - Reorganized index.html
    - Added google map to #map, however map no overlays on .main-container
    - Added sample map asisstance image for testing purposes
    - added api keys

--- app.js
+++ app.js
@@ -7,42 +7,66 @@ $(document).ready(function(){
 	var map = $("#map");
 	var locationList = $("#location-list");
 	var formContainer = $("#form-container");
+	var mapUrl = "https://maps.googleapis.com/maps/api/js?size=1920x1080&location=14612&pitch=45&key=AIzaSyCAYhJSCo97R9osuDm5D82SHs0oEJSzbk8";
+	var svKey = "AIzaSyCAYhJSCo97R9osuDm5D82SHs0oEJSzbk8";
+	var mapKey = "AIzaSyBk1lO9a-jKHIAPJLO0IG0vJ6cnwEkV5cQ";
 
 	/* -------------------------- App data model below ------------------------------ */
-	var model = function() {
-		// address: ko.observable(5),
-		this.address = address;
+	var model = {
+
 	};
 
 	/* -------------------------- App View below ------------------------------ */
 	var view = {
 
+		// InitPlaceholder: map.append("<img src='https://maps.googleapis.com/maps/api/streetview?size=900x300&location=Statue of liberty&pitch=45&key=AIzaSyCAYhJSCo97R9osuDm5D82SHs0oEJSzbk8'>")
+		// Initbackground: body.css('background-image', 'url()'),
+
 	};
 
 	/* -------------------------- App Controller below ------------------------------ */
-	var viewmodel = {
+	var viewmodel = function() {
 
-		testFunction: function() {
+		testFunction = function() {
 			console.log("chirp testFunction");
 			// console.log(model.address);
-		}
+			// map.append("<img src='AFR_Logo2.jpg'>");
+		};
 
+		mapFunction = function() {
+			console.log("map function called");
+			map.append();
+		};
+
+        // EXAMPLE.text("");
         // clear out old data before new request
 
-        // $wikiElem.text("");
-        // $nytElem.text("");
+        clickStuff = function() {
 
-        // load map?
+			console.log("chirp submit button click");
+			// body.empty();
+			// map.append
+		};
+
 	};
-
-	$("#submit-btn").click(function() {
-		console.log("button click line 42");
-	});
 
 	ko.applyBindings(viewmodel);
 
-	// formContainer.submit(viewmodel);
+	viewmodel();
 
-	// $("#submit-btn").submit(viewmodel);
+	mapFunction();
+
+	console.log(mapUrl);
+
+	// var map;
+	//   function initMap() {
+	//     map = new google.maps.Map(document.getElementById('map'), {
+	//       center: {lat: 40.719656, lng: -74.000781},
+	//       zoom: 18,
+	//       draggable: false,
+	//       noClear: true
+	//     });
+	//   }
+
 
 });
