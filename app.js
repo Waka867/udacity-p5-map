131e90d2ec0e18dc6334b37d48add0e86605a3e8
Waka867 <ledwing.hernandez@gmail.com>
2016-03-18 13:50:11 -0400

    js rearrangeing / trying to add infowindows

--- app.js
+++ app.js
@@ -3,23 +3,28 @@ var model = {
 	locations: [
 		{
 			name: "34 Howard Street",
-			position: {lat: 40.719656, lng: -74.000781}
+			position: {lat: 40.719656, lng: -74.000781},
+			posName: "mapPoint",
 		},
 		{
 			name: "222 West Broadway",
-			position: {lat: 40.719196, lng: -74.006356}
+			position: {lat: 40.719196, lng: -74.006356},
+			posName: "mapPoint2"
 		},
 		{
 			name: "5 Crosby Street",
-			position: {lat: 40.719606, lng: -74.000221}
+			position: {lat: 40.719606, lng: -74.000221},
+			posName: "mapPoint3"
 		},
 		{
 			name: "59 Elizabeth Street",
-			position: {lat: 40.717253, lng: -73.996556}
+			position: {lat: 40.717253, lng: -73.996556},
+			posName: "mapPoint4"
 		},
 		{
 			name: "271 Church Street",
-			position: {lat: 40.718268, lng: -74.005223}
+			position: {lat: 40.718268, lng: -74.005223},
+			posName: "mapPoint5"
 		}
 	]
 };
@@ -58,6 +63,11 @@ var mapPoint3name = model.locations[2].name;
 var mapPoint4name = model.locations[3].name;
 var mapPoint5name = model.locations[4].name;
 
+var pointList = model.locations;
+
+var i = i;
+// var loopMap = model.locations;
+
 function initMap() {
 
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -69,77 +79,61 @@ function initMap() {
 		mapTypeControl: false
 	});
 
-	var marker = new google.maps.Marker({
-		position: mapPoint,
-		map: map,
-		title: mapPointname,
-		// label: mapPointname,
-		draggable: true
-	});
+	for(i = 0; i < pointList.length; i++ ) {
+		marker = new google.maps.Marker({
+			position: pointList[i].position,
+			map: map,
+			title: pointList[i].name,
+			draggable: false,
+			animation: google.maps.Animation.DROP,
+			testMethod: function() {
+				addEventListener('click', console.log("test"))
+			}
+		});
 
-	var marker2 = new google.maps.Marker({
-		position: mapPoint2,
-		map: map,
-		title: mapPoint2name,
-		// label: mapPoint2name,
-		draggable: true
-	});
-
-	var marker3 = new google.maps.Marker({
-		position: mapPoint3,
-		map: map,
-		title: mapPoint3name,
-		// label: mapPoint3name,
-		draggable: true
-	});
-
-	var marker4 = new google.maps.Marker({
-		position: mapPoint4,
-		map: map,
-		title: mapPoint4name,
-		// label: mapPoint4name,
-		draggable: true
-	});
-
-	var marker5 = new google.maps.Marker({
-		position: mapPoint5,
-		map: map,
-		title: mapPoint5name,
-		// label: mapPoint5name,
-		draggable: true
-	});
-
-	function toggleBounce() {
-		if (marker.getAnimation() !== null) {
-			marker.setAnimation(null);
-		} else {
-			marker.setAnimation(google.maps.Animation.BOUNCE);
+		function toggleBounce() {
+			if (marker.getAnimation() !== null) {
+				marker.setAnimation(null);
+			} else {
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+			};
 		};
+
+		google.maps.event.addListener(marker, 'click', function(){
+			toggleBounce();
+		});
+
+		var infobox = "<h1>" + pointList[i].name + "</h1>";
+
+		console.log(infobox); /* Test */
 	};
 
-	var infobox = "<h1>" + mapPointname + "</h1>";
+	console.log(pointList); /* Test */
+	console.log(pointList[1].name); /* Test */
 
-	var infowindow = new google.maps.InfoWindow({
-		content: infobox
-	});
+	// marker.addListener('click', function() {
+	// 	InfoWindow.open(map, marker);
+	// 	toggleBounce();
+	// });
 
-	marker.addListener('click', function() {
-		infowindow.open(map, marker);
-		toggleBounce();
-	});
+	// var infowindow = new google.maps.InfoWindow({
+	// 	content: infobox
+	// });
+
+	// var infobox = "<h1>" + mapPointname + "</h1>";
 };
 
 
-var body = $("body");
-var address = $("#address").val();
-var zip = $("#zip").val();
-var state = $("#state").val();
+// var body = $("body");
+// var address = $("#address").val();
+// var zip = $("#zip").val();
+// var state = $("#state").val();
 var mapArea = $("#map");
-var locationList = $("#location-list");
-var formContainer = $("#form-container");
-var mapUrl = "https://maps.googleapis.com/maps/api/js?size=1920x1080&location=14612&pitch=45&key=AIzaSyCAYhJSCo97R9osuDm5D82SHs0oEJSzbk8";
-var svKey = "AIzaSyCAYhJSCo97R9osuDm5D82SHs0oEJSzbk8";
-var mapKey = "AIzaSyBk1lO9a-jKHIAPJLO0IG0vJ6cnwEkV5cQ";
+// var locationList = $("#location-list");
+// var formContainer = $("#form-container");
+// var mapUrl = "https://maps.googleapis.com/maps/api/js?size=1920x1080&location=14612&pitch=45&key=AIzaSyCAYhJSCo97R9osuDm5D82SHs0oEJSzbk8";
+// var svKey = "AIzaSyCAYhJSCo97R9osuDm5D82SHs0oEJSzbk8";
+// var mapKey = "AIzaSyBk1lO9a-jKHIAPJLO0IG0vJ6cnwEkV5cQ";
 
 
 $(document).ready(function(){
@@ -151,7 +145,5 @@ $(document).ready(function(){
 	view();
 
 	mapFunction();
-
-	console.log(model.locations[2].position);/* TEST FUNCTION ----------------     --------------    TEST FUNCTION */
 
 });
