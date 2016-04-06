ca61eae46a74e0f67e240bb6ec473db2608f242b
Waka867 <ledwing.hernandez@gmail.com>
2016-04-05 21:01:30 -0400

    wiki api / refactoring / clean up
    - Added address key/value to each location object
    - Moved wiki url and ajax request into viewModel / click event listener
    so the ajax request only runs when a user clicks, not when markers are
    created
    - Added wiki api info to infowindow, including summary and link
    - Removed some unused lines of code and commented areas

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 /* -------------------------- App data model below ------------------------------ */
-// var clientId = "RS2OHMJ3OBNBCLAXXX0BZCPSQ4P2N3ZFVJ2YW1ZUUYBL5WPC";
-// var	clientSecret = "R3PRHQAUQZ4H5WADS5SL03PNUSN4DQOXEDZ01H5L2EP1PY5E";
-// var urlCombo = "https://api.foursquare.com/v2/venues/search?client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20130806&ll=" + this.position + this.position + "&query=" + this.title;
-
 var model = {
 	locations: [
 		{
 			name: "New York Public Library",
 			position: {lat: 40.7135559, lng: -74.0077815},
 			posName: "mapPoint",
-			// infoUrl: urlCombo,
+			address: "9 Murray St",
 			infoContent: "<h1>9 Murray St</h1><p class='wikiStuff'></p>"
 		},
 		{
 			name: "New York City Hall",
 			position: {lat: 40.712705, lng: -74.0059778},
 			posName: "mapPoint2",
-			infoContent: "<h1>City Hall Pk</h1><p class='Stuff'></p>"
+			address: "City Hall Pk",
+			infoContent: "<h1>City Hall Pk</h1><p class='wikiStuff'></p>"
 		},
 		{
-			name: "The Woolworth Building",
+			name: "Woolworth Building",
 			position: {lat: 40.7123824, lng: -74.0081218},
 			posName: "mapPoint3",
-			infoContent: "<h1>233 Broadway</h1><p class='Stuff'></p>"
+			address: "233 Broadway, 10007",
+			infoContent: "<h1>233 Broadway</h1><p class='wikiStuff'></p>"
 		},
 		{
 			name: "One World Trade Center",
 			position: {lat: 40.713154, lng: -74.013661},
 			posName: "mapPoint4",
-			infoContent: "<h1>285 Fulton St</h1><p class='Stuff'></p>"
+			address: "285 Fulton St",
+			infoContent: "<h1>285 Fulton St</h1><p class='wikiStuff'></p>"
 		},
 		{
 			name: "St. Peter's Roman Catholic Church",
 			position: {lat: 40.7123014, lng: -74.0095246},
 			posName: "mapPoint5",
-			infoContent: "<h1>22 Barclay St</h1><p class='Stuff'></p>"
+			address: "22 Barclay St",
+			infoContent: "<h1>22 Barclay St</h1><p class='wikiStuff'></p>"
 		}
 	]
 };
@@ -47,8 +47,6 @@ var view = {
 
 	// var locArray = ko.observableArray(locArray);
 	// var query = ko.observable('');
-
-	// console.log("View initialized");
 
 	// var search = function(value) {
 	// 	view.locArray.removeAll(); // list removal
@@ -88,6 +86,7 @@ var viewModel = {
 				draggable: false,
 				info: locArray[i].infoContent,
 				animation: google.maps.Animation.DROP,
+				address: locArray[i].address
 			});
 
 			console.log(markers[i].title); /* test - NOTE: log lists all address correctly */
@@ -113,8 +112,28 @@ var viewModel = {
 				};
 				infowindow.setContent(this.info);
 				infowindow.open(map, this);
-				// console.log(this.info);
+
+				var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.title + '&format=json';
+
+				console.log(this.title);
 				// console.log(infowindow);
+
+				var ajax = $.ajax({
+					url: wikiUrl,
+					dataType: "jsonp",
+					headers: { 'Api-User-Agent': 'Example/1.0' }
+				}).done(function(response) {
+					var respLength = response.length;
+					var respTitle = response[0];
+					var respSumm = response[2];
+					var wikiLinks = response[3];
+
+					console.log(response);
+					console.log(wikiUrl);
+
+					/* Appends wikipedia sourced info to the infowindow via the model and each locations infoContent key */
+					$(".wikiStuff").append("<h3>" + respTitle + "</h3><p>" + respSumm[0] + "</p><p><a href=" + wikiLinks[0] + " target='_blank'>Learn more about " + respTitle + " here</a></p>");
+				});
 			});
 		};
 
@@ -146,10 +165,8 @@ var starter = function() {
 			mapTypeControl: false
 	});
 
-	// viewModel.map();
 	viewModel.markerMaker();
 	ko.applyBindings(viewModel);
-	// viewModel.markerMaker(); /* Was causing double actions because markerMaker runs automatically */
 };
 
 $(document).ready(function(){
